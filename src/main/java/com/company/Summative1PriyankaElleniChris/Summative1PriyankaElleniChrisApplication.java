ad897acd318507f97017832eaab48a90d860d825
KreshsDesktop <ckresh99@gmail.com>
2022-10-17 16:46:14 -0400

    first commit

---
+++ src/main/java/com/company/Summative1PriyankaElleniChris/Summative1PriyankaElleniChrisApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative1PriyankaElleniChris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1PriyankaElleniChrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1PriyankaElleniChrisApplication.class, args);
+	}
+
+}
