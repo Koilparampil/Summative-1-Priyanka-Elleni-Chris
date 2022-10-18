10ffcba3a6a3142ae121dcaafcd9538f3d8bc784
KreshsDesktop <ckresh99@gmail.com>
2022-10-18 13:10:56 -0400

    asdded valid to two routes

--- src/main/java/com/company/Summative1PriyankaElleniChris/controller/ConsoleController.java
+++ src/main/java/com/company/Summative1PriyankaElleniChris/controller/ConsoleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,12 +37,12 @@ public class ConsoleController {
     }
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public Console addConsole(@RequestBody Console console) {
+    public Console addConsole(@RequestBody @Valid Console console) {
         return consoleRepository.save(console);
     }
     @PutMapping()
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateConsole(@RequestBody Console console) {
+    public void updateConsole(@RequestBody @Valid Console console) {
         consoleRepository.save(console);
     }
     @DeleteMapping("/{id}")
