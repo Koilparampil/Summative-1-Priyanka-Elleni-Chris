fe5c31c1a122939eea1385d45f7f5bafe99a9abc
Elleni Tessema <ellenitessema@Ellenis-MacBook-Pro.local>
2022-10-18 12:12:27 -0400

    adding repository anotations

--- src/main/java/com/company/Summative1PriyankaElleniChris/repository/SalesTaxRatRepository.java
+++ src/main/java/com/company/Summative1PriyankaElleniChris/repository/SalesTaxRatRepository.java
@@ -2,7 +2,9 @@ package com.company.Summative1PriyankaElleniChris.repository;
 
 import com.company.Summative1PriyankaElleniChris.model.SalesTaxRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SalesTaxRatRepository extends JpaRepository<SalesTaxRate, String > {
 
 
--- src/main/java/com/company/Summative1PriyankaElleniChris/repository/T_shirtRepository.java
+++ src/main/java/com/company/Summative1PriyankaElleniChris/repository/T_shirtRepository.java
@@ -2,9 +2,11 @@ package com.company.Summative1PriyankaElleniChris.repository;
 
 import com.company.Summative1PriyankaElleniChris.model.T_shirt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface T_shirtRepository extends JpaRepository<T_shirt, Integer> {
     List<T_shirt> findT_shirtByColor(String color);
     List<T_shirt> findT_shirtBySize(String size);
