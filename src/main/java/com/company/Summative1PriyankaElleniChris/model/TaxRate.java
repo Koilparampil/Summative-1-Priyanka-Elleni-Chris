b8e55cc7408a587ee6c179b589d347cbec1c2265
Elleni Tessema <ellenitessema@Ellenis-MacBook-Pro.local>
2022-10-18 12:02:24 -0400

    adding SalesTaxRate model and repository

--- src/main/java/com/company/Summative1PriyankaElleniChris/model/TaxRate.java
+++
@@ -1,9 +0,0 @@
-package com.company.Summative1PriyankaElleniChris.model;
-
-import javax.persistence.Entity;
-
-@Entity
-
-public class TaxRate {
-
-}
---
+++ src/main/java/com/company/Summative1PriyankaElleniChris/repository/SalesTaxRatRepository.java
@@ -0,0 +1,9 @@
+package com.company.Summative1PriyankaElleniChris.repository;
+
+import com.company.Summative1PriyankaElleniChris.model.SalesTaxRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalesTaxRatRepository extends JpaRepository<SalesTaxRate, String > {
+
+
+}
