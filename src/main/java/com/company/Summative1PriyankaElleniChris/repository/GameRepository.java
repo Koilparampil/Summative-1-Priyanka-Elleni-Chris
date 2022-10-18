99187487e2bc4f72d096ef355dcb6bc591a116a7
marycpriyanka <marycpriyanka@gmail.com>
2022-10-18 09:18:35 -0700

    Add game repository

--- src/main/java/com/company/Summative1PriyankaElleniChris/repository/GameRepository.java
+++ src/main/java/com/company/Summative1PriyankaElleniChris/repository/GameRepository.java
@@ -1,5 +1,13 @@
 package com.company.Summative1PriyankaElleniChris.repository;
 
 
-public class GameRepository {
+import com.company.Summative1PriyankaElleniChris.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GameRepository extends JpaRepository<Game, Integer> {
+    public List<Game> findByStudio(String studio);
+    public List<Game> findByEsrbRating(String esrbRating);
+    public List<Game> findByTitle(String title);
 }
