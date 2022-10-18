8ebee5aa3248ee9132cbec5cc5f4135e21eddaf9
KreshsDesktop <ckresh99@gmail.com>
2022-10-18 13:01:22 -0400

    added ConsoleController and edited console model and ConsoleRepository to round out the Console controller.

---
+++ src/main/java/com/company/Summative1PriyankaElleniChris/controller/ConsoleController.java
@@ -0,0 +1,52 @@
+package com.company.Summative1PriyankaElleniChris.controller;
+
+import com.company.Summative1PriyankaElleniChris.model.Console;
+import com.company.Summative1PriyankaElleniChris.repository.ConsoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/consoles")
+public class ConsoleController {
+
+    @Autowired
+    ConsoleRepository consoleRepository;
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<Console> getConsoles(){
+        return consoleRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Console getSingleConsole(@PathVariable Integer id){
+        Optional<Console> returnVal = consoleRepository.findById(id);
+        return returnVal.orElse(null);
+    }
+    @GetMapping("manufacturer/{manufacturer}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Console> getConsolesByManufacturer(@PathVariable String manufacturer){
+        List<Console>  returnVal = consoleRepository.findByManufacturer(manufacturer);
+        return returnVal;
+    }
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Console addConsole(@RequestBody Console console) {
+        return consoleRepository.save(console);
+    }
+    @PutMapping()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateConsole(@RequestBody Console console) {
+        consoleRepository.save(console);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteConsole(@PathVariable int id){
+        consoleRepository.deleteById(id);
+    }
+}
