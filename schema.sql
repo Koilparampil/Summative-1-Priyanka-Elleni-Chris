ffe819aae295318a25f7e908220fb4cc3cde88f1
KreshsDesktop <ckresh99@gmail.com>
2022-10-20 10:22:47 -0400

    changes to the SQL query

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+create schema if not exists summative_1_project;
+use summative_1_projectconsole;
 create table if not exists game (
     game_id int not null auto_increment primary key,
     title varchar(50) not null,
@@ -57,3 +59,63 @@ create table if not exists invoice (
     processing_fee decimal (5,2) not null,
     total decimal(5,2) not null
 );
+INSERT INTO `summative_1_project`.`processing_fee`(`product_type`, `fee`)
+VALUES
+('Conosle','14.99'),
+('Game','1.49'),
+('T-Shirt','1.98');
+
+;
+INSERT INTO `summative_1_project`.`sales_tax_rate` (`state`, `rate`) 
+VALUES 
+('AL', '.05'),
+('AK', '.06'),
+('AZ', '.04'),
+('AR', '.06'),
+('CA', '.06'),
+('CO', '.04'),
+('CT', '.03'),
+('DE', '.05'),
+('FL', '.06'),
+('GA', '.07'),
+('HI', '.05'),
+('ID', '.03'),
+('IL', '.05'),
+('IN', '.05'),
+('IA', '.04'),
+('KS', '.06'),
+('KY', '.04'),
+('LA', '.05'),
+('ME', '.03'),
+('MD', '.07'),
+('MA', '.05'),
+('MI', '.06'),
+('MN', '.06'),
+('MS', '.05'),
+('MO', '.05'),
+('MT', '.03'),
+('NE', '.04'),
+('NV', '.04'),
+('NH', '.06'),
+('NJ', '.05'),
+('NM', '.05'),
+('NY', '.06'),
+('NC', '.05'),
+('ND', '.05'),
+('OH', '.04'),
+('OK', '.04'),
+('OR', '.07'),
+('PA', '.06'),
+('RI', '.06'),
+('SC', '.06'),
+('SD', '.06'),
+('TM', '.05'),
+('TX', '.03'),
+('UT', '.04'),
+('VT', '.07'),
+('VA', '.06'),
+('WA', '.05'),
+('WV', '.05'),
+('WI', '.03'),
+('WY', '.04');
+
