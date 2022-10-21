782c08747078cbbc982c1f23df4085a5f58bcb5f
marycpriyanka <marycpriyanka@gmail.com>
2022-10-20 21:13:00 -0700

    Add game interface functionality

--- FrontEnd/game-store-front-end/src/pages/Game.jsx
+++ FrontEnd/game-store-front-end/src/pages/Game.jsx
@@ -1,45 +1,202 @@
-import React, { useState, useEffect } from 'react';
-import GameCard from './components/GameCard';
+import React, { useState, useEffect } from "react";
+import GameCard from "./components/GameCard";
+import GameForm from "./components/GameForm";
 
 const Game = () => {
   const [games, setGames] = useState([]);
   const [error, setError] = useState();
+  const [title, setTitle] = useState("");
+  const [studio, setStudio] = useState("");
+  const [scopedGame, setScopedGame] = useState();
+  const [showForm, setShowForm] = useState();
 
+  // Get all games
   useEffect(() => {
     fetch("http://localhost:8080/games")
-    .then(response => response.json())
-    .then(result => setGames(result))
-    .catch(console.log);
-}, []);
+      .then((response) => response.json())
+      .then((result) => setGames(result))
+      .catch(console.log);
+  }, []);
 
-  function notify() {
+  //
+  const addClick = () => {
+    setScopedGame({ gameId: 0, title: "", esrbRating: "E", description: "", price: 0, studio: "", quantity: 0 });
+    setShowForm(true);
+  };
 
+  // Gets games by ESRB
+  const fetchByEsrbRating = (event) => {
+    if (event.target.value === "") {
+      fetch("http://localhost:8080/games")
+        .then((response) => response.json())
+        .then((result) => setGames(result))
+        .catch(console.log);
+    } else {
+      fetch("http://localhost:8080/games/rating/" + event.target.value)
+        .then((response) => response.json())
+        .then((result) => setGames(result))
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const handleTitleChange = (event) => {
+    const { target } = event;
+    setTitle(target.value);
+  };
+
+  // Get games by title
+  const handleTitleSearch = (event) => {
+    // Prevents the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+
+    if (!title) {
+      fetch("http://localhost:8080/games")
+        .then((response) => response.json())
+        .then((result) => setGames(result))
+        .catch(console.log);
+    } else {
+      fetch("http://localhost:8080/games/title/" + title)
+        .then((response) => response.json())
+        .then((result) => setGames(result))
+        .catch((err) => console.log(err));
+    }
+
+    setTitle("");
+  };
+
+  const handleStudioChange = (event) => {
+    const { target } = event;
+    setStudio(target.value);
+  };
+
+  // Get games by title
+  const handleStudioSearch = (event) => {
+    // Prevents the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+
+    if (!studio) {
+      fetch("http://localhost:8080/games")
+        .then((response) => response.json())
+        .then((result) => setGames(result))
+        .catch(console.log);
+    } else {
+      fetch("http://localhost:8080/games/studio/" + studio)
+        .then((response) => response.json())
+        .then((result) => setGames(result))
+        .catch((err) => console.log(err));
+    }
+
+    setStudio("");
+  };
+
+  function notify({ action, game, error }) {
+    if (error) {
+      console.log(error);
+      setError(error);
+      setShowForm(false);
+      return;
+    }
+
+    switch (action) {
+      case "add":
+        setGames([...games, game]);
+        break;
+      case "edit":
+        setGames(
+          games.map((g) => {
+            if (g.gameId === game.gameId) {
+              return game;
+            }
+            return g;
+          })
+        );
+        break;
+      case "edit-form":
+        setScopedGame(game);
+        setShowForm(true);
+        return;
+      case "delete":
+        setGames(
+          games.filter((g) => g.gameId !== game.gameId)
+        );
+        break;
+      default:
+        break;
+    }
+
+    setError("");
+    setShowForm(false);
+  }
+
+  if (showForm) {
+    return <GameForm game={scopedGame} notify={notify}></GameForm>;
   }
 
   return (
     <div>
-      <h1 id='gameTitle'>Games</h1>
+      <h1 id="gameTitle">Games</h1>
 
-      {/* <button className="btn btn-primary" type="button" onClick={addClick}>Add a Recipe</button> */}
+      <div>
+        <button className="btn btn-primary" type="button" onClick={addClick}>
+          Add a game
+        </button>
 
-      <table id='games'>
+        <select name="esrbRating" onChange={fetchByEsrbRating}>
+          <option value="">Get Games by ESRB Rating</option>
+          <option value="E">E</option>
+          <option value="E10+">E10+</option>
+          <option value="T">T</option>
+          <option value="M">M</option>
+          <option value="AO">AO</option>
+        </select>
+
+        <form>
+          <input
+            value={title}
+            name="title"
+            onChange={handleTitleChange}
+            type="text"
+            placeholder="Get games by title"
+          ></input>
+          <button type="button" onClick={handleTitleSearch}>
+            Search
+          </button>
+        </form>
+
+        <form>
+          <input
+            value={studio}
+            name="studio"
+            onChange={handleStudioChange}
+            type="text"
+            placeholder="Get games by studio"
+          ></input>
+          <button type="button" onClick={handleStudioSearch}>
+            Search
+          </button>
+        </form>
+      </div>
+
+      <table id="games">
         <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>ESRB rating</th>
-                        <th>Desription</th>
-                        <th>Price</th>
-                        <th>Studio</th>
-                        <th>Quantity</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {games.map(game => <GameCard key={game.gameId} game={game} notify={notify} />)}
-                    </tbody>
-                </table>
+          <tr>
+            <th>Title</th>
+            <th>ESRB rating</th>
+            <th>Desription</th>
+            <th>Price</th>
+            <th>Studio</th>
+            <th>Quantity</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {games.map((game) => (
+            <GameCard key={game.gameId} game={game} notify={notify} />
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
 export default Game;
---
+++ FrontEnd/game-store-front-end/src/pages/components/GameForm.js
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import Game from "../Game";
+
+function GameForm({game: initialGame, notify }) {
+    const [game, setGame] = useState(initialGame);
+    const isAdd = initialGame.gameId === 0;
+
+    function handleChange(event) {
+        const clone = { ...game };
+        clone[event.target.name] = event.target.value;
+        setGame(clone);
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+
+        const url = `http://localhost:8080/games`;
+        const method = isAdd ? "POST" : "PUT";
+        const expectedStatus = isAdd ? 201 : 204;
+
+        const init = {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(game)
+        };
+
+        fetch(url, init)
+            .then(response => {
+
+                if (response.status === expectedStatus) {
+                    if (isAdd) {
+                        return response.json();
+                    } else {
+                        return game;
+                    }
+                }
+                return Promise.reject(`Didn't receive expected status: ${expectedStatus}`);
+            })
+            .then(result => notify({
+                action: isAdd ? "add" : "edit",
+                game: result
+            }))
+            .catch(error => notify({ error: error }));
+    }
+
+    return (
+        <>
+            <h1>{isAdd ? "Add" : "Edit"} Game</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" id="title" name="title"
+                        className="form-control"
+                        value={game.title} onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                <label htmlFor="esrbRating">ESRB Rating</label>
+                    <select name="esrbRating" value={game.esrbRating} onChange={handleChange}>
+                    <option value="E">E</option>
+                    <option value="E10+">E10+</option>
+                    <option value="T">T</option>
+                    <option value="M">M</option>
+                    <option value="AO">AO</option>
+                    </select>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="description">Description</label>
+                    <input type="text" id="description" name="description"
+                        className="form-control"
+                        value={game.description} onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="price">Price</label>
+                    <input type="text" id="price" name="price"
+                        className="form-control"
+                        value={game.price} onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="studio">Studio</label>
+                    <input type="text" id="studio" name="studio"
+                        className="form-control"
+                        value={game.studio} onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="quantity">Quantity</label>
+                    <input type="text" id="quantity" name="quantity"
+                        className="form-control"
+                        value={game.quantity} onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <button id="saveButton" className="btn btn-primary mr-3" type="submit">Save</button>
+                    <button className="btn btn-secondary" type="button" onClick={() => notify({ action: "cancel" })}>Cancel</button>
+                </div>
+            </form>
+        </>
+    );
+}
+
+export default GameForm;
